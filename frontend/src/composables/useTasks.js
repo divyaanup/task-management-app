897abc4c22ca68f350010d7b9d7b510a94fa492b
Divya <divya@jdmapp.com>
2026-05-24 15:49:24 +0100

    implement task manage functions using composables

--- frontend/src/composables/useTasks.js
+++ frontend/src/composables/useTasks.js
@@ -6,4 +6,53 @@ const editingTask = ref(null)
 
 export function useTasks() {
 
+    const fetchTasks = async (status = '') => {
+
+        let url = '/?path=tasks'
+
+        if (status) {
+        url += `&status=${status}`
+        }
+
+        const response = await api.get(url)
+
+        tasks.value = response.data
+    }
+
+    const startEdit = (task) => {
+        editingTask.value = { ...task }
+    }
+
+    const createTask = async (task) => {
+
+        await api.post('/?path=tasks', task)
+
+        await fetchTasks()
+    }
+
+    const updateTask = async (task) => {
+
+        await api.put('/?path=tasks', task)
+
+        editingTask.value = null
+
+        await fetchTasks()
+    }
+
+    const deleteTask = async (id) => {
+
+        await api.delete(`/?path=tasks&id=${id}`)
+
+        await fetchTasks()
+    }
+
+    return {
+        tasks,
+        fetchTasks,
+        createTask,
+        startEdit,
+        updateTask,
+        deleteTask,
+        editingTask
+    }
 }
